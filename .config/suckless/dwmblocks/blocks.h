eed2ce48ffcb2394a0e7347e2b1ae68a5f56814f
Alexander Zerpa <alexzerpawanner@gmail.com>
2022-07-08 04:34:20 +0200

    migrated all suckless config to config directory changed picom fork to picom-animations-git for better animations added slock support changed install-basics script to reflect the 3 changes above fix screenshot-maim script
        (from now on all future suckless configuration will be manage by dotfiles)

---
+++ .config/suckless/dwmblocks/blocks.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		    /*Update Interval*/	/*Update Signal*/
+	{"",        "sb-pacpackages",   0,                   8},
+	{"",        "sb-volume",        0,                   2},
+	{"",        "sb-clock",         30,                  1},
+	{"",        "sb-layout",        0,                   10},
+	{"",        "sb-internet",      60,                  4},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
